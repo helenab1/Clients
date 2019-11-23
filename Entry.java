da424003c46b923957c0f2e8f6988b54f44f6a48
helenab1 <31667934+helenab1@users.noreply.github.com>
2019-11-23 01:07:43 -0800

    Add files via upload

---
+++ Entry.java
@@ -0,0 +1,25 @@
+package Main;
+
+public class Entry {
+	
+	private int clientID;	// will get from client
+	private Date date;
+	private String facility;
+	private String entry;
+	
+
+	public Entry(int clientID, Date date, String facility, String entry) {
+		this.clientID = clientID;
+		this.date = date;
+		this.facility = facility;
+		this.entry = entry;
+	}
+		
+	
+
+	
+}
+	
+	
+
+
